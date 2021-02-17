1026186284b93aa680f5951342ca1ee0f9785fb1
K_T <armn7guc.supbs.3@gmail.com>
2021-02-17 22:29:05 +0900

    サンプルファイル作成

--- backend/resources/views/boot_template/index.blade.php
+++ backend/resources/views/boot_template/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.master_bootstrap') {{-- テンプレート読み込み --}}
-@section('title', 'KYOUYU-') {{-- サイトタイトル定義 --}}
+@section('title', 'Kyouyu-') {{-- サイトタイトル定義 --}}
 @section('content')
     <!-- Page Content -->
     <div id="page-content">
         <div class="container text-center">
             <div class="row justify-content-center">
                 <div class="col-md-7">
-                    <h1 class="font-weight-light mt-4 text-white">Bootstrap Startup kit</h1>
-                    <p class="lead text-white-50">
+                    <h1 class="font-weight-light mt-4 .text-dark">Bootstrap Startup kit</h1>
+                    <p class="lead .text-dark">
                         このページでは「Bootstrap Snippets」の「Flexbox Sticky Footer」と「Bootstrap4 Navbar with Logo Image」を使って構成しています。<br><br>
                         Laravelの学習の際にデモページとしてお使いください。
                     </p>
--- backend/resources/views/layouts/header_bootstrap.blade.php
+++ backend/resources/views/layouts/header_bootstrap.blade.php
@@ -1,8 +1,8 @@
 <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+<nav class="navbar navbar-expand-lg alert-danger alert-danger static-top">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="http://placehold.it/150x50?text=Logo" alt="">
+            <img src="http://placehold.it/150x50?text=Laravel学習用" alt="Laravel学習用">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
