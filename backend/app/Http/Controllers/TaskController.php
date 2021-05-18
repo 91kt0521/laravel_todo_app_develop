f1517b57d98e35c7efd71cad919be6c5e980b2a5
K_T <armn7guc.supbs.3@gmail.com>
2021-05-18 23:43:46 +0900

    [add]:タスク更新API実装

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -19,4 +19,10 @@ class TaskController extends Controller
     public function store(Request $request) {
         return Task::create($request->all());
     }
+
+    public function update(Request $request, Task $task) {
+        $task->update($request->all());
+
+        return $task;
+    }
 }
